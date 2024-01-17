aa91d8e2cf5aa50c0fac724b8410bee26cb61dde
adnanolarmmi <olarmmi@gmail.com>
2024-01-18 00:32:26 +0100

    Wiki home page

--- produce.rb
+++ produce.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 
 get("/apple") do
-    "Here's a juicy apple"
+  "<h1>Here's a juicy apple</h1>"
+end
+
+get("/banana") do
+    "<h1>Here is a banana</h1>"
+end
+
+get("/carrot") do
+    "<h1>This is a carrot</h1>"
 end
