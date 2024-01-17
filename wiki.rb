c9cc25cf8150606d21338eb5790848dab826e307
adnanolarmmi <olarmmi@gmail.com>
2024-01-18 00:42:21 +0100

    Preview for path

--- wiki.rb
+++ wiki.rb
@@ -1,5 +1,17 @@
 require "sinatra"
 
+def page_content(title)
+    File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+    return nil
+end
+
+puts page_content("John Doe")
+
 get "/" do
   erb :welcome
+end
+
+get "/:title" do
+  page_content(params[:title])  
 end
